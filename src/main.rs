b5c840eac8dc4406793933f9f4ba9b0f82248c0c
Tom Selfin <selfint@gmail.com>
2020-12-09 23:25:06 +0200

    clear main

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,4 @@ mod lexer;
 mod parser;
 mod reader;
 
-use lexer::Lexer;
-use std::env;
-use std::fs;
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let filename = &args[1];
-
-    println!("Checking file {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-
-    let tokenizer = Lexer::new();
-
-    for line in contents.lines() {
-        for word in line.split_ascii_whitespace() {
-            if let Ok(word_token) = tokenizer.convert_word_to_token(word) {
-                println!("Word: {}, token: {:?}", word, word_token);
-            } else {
-                panic!("Unknown word: '{}', exiting!", word);
-            }
-        }
-    }
-}
+fn main() {}
