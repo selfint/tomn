a33bc1e2fb1cd654cccf9a04e68cd50898eec8da
Tom Selfin <selfint@gmail.com>
2020-12-09 23:26:02 +0200

    clear keyword_consts

--- src/keyword_consts.rs
+++
@@ -1,4 +0,0 @@
-pub(crate) const TRUE_KEYWORD: &str = "true";
-pub(crate) const FALSE_KEYWORD: &str = "false";
-pub(crate) const OR_KEYWORD: &str = "or";
-pub(crate) const AND_KEYWORD: &str = "and";
