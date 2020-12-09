bbf090d854463b65f8d56d4fa1cc7848f4e2096a
Tom Selfin <selfint@gmail.com>
2020-12-09 18:56:57 +0200

    print all tokens in file, crash on tokenization fail

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
 mod keyword_consts;
 mod tokenizer;
+use std::env;
+use std::fs;
+use tokenizer::Tokenizer;
 
-fn main() {}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+
+    println!("Checking file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    let tokenizer = Tokenizer;
+
+    for line in contents.lines() {
+        for word in line.split_ascii_whitespace() {
+            if let Ok(word_token) = tokenizer.convert_word_to_token(word) {
+                println!("Word: {}, token: {:?}", word, word_token);
+            } else {
+                panic!("Unknown word: '{}', exiting!", word);
+            }
+        }
+    }
+}
